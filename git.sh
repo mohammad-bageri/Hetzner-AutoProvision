d02c4b54302a53acc3cddc8fc4e0dc4f2a4e67af
mbageri <mohammadbageri@gmail.com>
2019-02-18 20:29:37 +0000

    bugfix

--- git.sh
+++ git.sh
@@ -3,7 +3,7 @@
 echo 1 >> 1.txt
 git add .
 
-for number in $(seq 1200 1500 | shuf | tail -n 1);do
+for number in $(seq 1400 1700 | shuf | tail -n 1);do
 	git commit --date "$number day ago" -m "bugfix" .
        
 done      
