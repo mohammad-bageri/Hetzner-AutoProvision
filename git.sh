00ffbe6dca872c34bfa1c34041400eae406fc63a
mbageri <mohammadbageri@outlook.com>
2020-02-23 20:22:07 +0000

    bugfix

--- git.sh
+++ git.sh
@@ -6,4 +6,6 @@ git add .
 for number in $(seq 1200 1500 | shuf | tail -n 1);do
 	git commit --date "$number day ago" -m "bugfix" .
        
-done       
+done      
+
+echo "--------------commit done on $number day's ago!-----------------"
